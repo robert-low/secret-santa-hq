815a7f2d1e4c063c89028a78b4db0c94d249c195
Robert Low <robert.low91@gmail.com>
2022-12-14 11:59:31 +0000

    wishlist model and migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_113924) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_115701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,4 +32,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_113924) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wishlists", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_wishlists_on_item_id"
+  end
+
+  add_foreign_key "wishlists", "items"
 end
